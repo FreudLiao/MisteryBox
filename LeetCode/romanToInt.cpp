dbb6fb0338cbd267523d790853632f75ec27189c
小朋友，你是不是有很多問號 <165641772+FreudLiao@users.noreply.github.com>
2026-01-19 19:17:10 +0800

    Create romanToInt.cpp

---
+++ LeetCode/romanToInt.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> roman{
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}           
+        };
+        int sum = 0;
+        int n = s.size();
+        for(int i = 0; i < n ; i++){
+            int a = roman[s[i]];
+            if(a < roman[s[i + 1]]){
+                sum -= a;
+            }else{
+                sum += a;// 累加
+            }
+        }
+        return sum;
+    }
+};
