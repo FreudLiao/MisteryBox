524d598b45f92c91a1b534eb1457f4dbb81c863a
小朋友，你是不是有很多問號 <165641772+FreudLiao@users.noreply.github.com>
2026-01-23 16:54:42 +0800

    Create longgestCommonPrefix.cpp

---
+++ LeetCode/longgestCommonPrefix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()){
+            return "";
+        }
+        int n = strs.size();
+        for(int i = 0; i < strs[0].size(); i++){
+            char c = strs[0][i];
+            for(int j = 1; j < n; j++){
+                if(strs[j][i] != c){
+                    return strs[0].substr(0, i);
+                    // slice word in piece (substr)
+                }
+            }
+        }
+        return strs[0];
+    }
+};
